49b04e97b3b4d6b364b1f47e8f8e4f51422036c2
Alejandro Ortiz <alejandro_ortiz426@hotmail.com>
2024-07-12 12:06:06 -0600

    test

--- app/Http/Livewire/Material/BitacoraMateriales.php
+++ app/Http/Livewire/Material/BitacoraMateriales.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Material;
 use App\Models\Entrada;
 use App\Models\EntradaMaterial;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -42,13 +43,12 @@ class BitacoraMateriales extends Component
         [$start, $end] = Entrada::getDateRange($this->year, $this->weekStart, $this->weekEnd);
         
         $materiales = EntradaMaterial::orderBy('created_at', 'desc')
+        ->select(DB::raw('material_id, sum(cantidad) as cantidad, sum(precio * cantidad) as importe'))
         ->whereBetween('created_at', [$start, $end]);
 
         if($this->desglosar){
-            // $materiales = $materiales->whereHas('material', function($query){
-            //     $query->where('desglosar', true);
-            // });
             $materiales->groupBy('material_id');
+
         }
 
         $this->totalMateriales = collect($materiales->get())->sum('importe');
