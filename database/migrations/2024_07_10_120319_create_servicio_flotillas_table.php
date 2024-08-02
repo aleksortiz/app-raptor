322043cfacde4152c820cb803c35b156ab96cfbd
Alejandro Ortiz <alejandro_ortiz426@hotmail.com>
2024-08-02 16:08:18 -0600

    api flotilla

--- database/migrations/2024_07_10_120319_create_servicio_flotillas_table.php
+++ database/migrations/2024_07_10_120319_create_servicio_flotillas_table.php
@@ -21,11 +21,6 @@ class CreateServicioFlotillasTable extends Migration
             $table->dateTime('fecha_servicio');
             $table->decimal('costo', 10, 2);
             $table->integer('kilometraje')->nullable();
-            $table->string('ubicacion');
-            $table->string('estatus_servicio')->default('PENDIENTE');
-            $table->dateTime('fecha_concluido')->nullable();
-            $table->string('tecnico_asignado')->nullable();
-            $table->text('observaciones')->nullable();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2024_08_02_160630_alter_servicio_flotillas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterServicioFlotillasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('personal', function (Blueprint $table) {
+            $table->string('ubicacion');
+            $table->string('estatus_servicio')->default('PENDIENTE');
+            $table->dateTime('fecha_concluido')->nullable();
+            $table->string('tecnico_asignado')->nullable();
+            $table->text('observaciones')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
