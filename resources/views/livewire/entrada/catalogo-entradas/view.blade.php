188b2d2f311bc51de20654ba4b3472d19d6d2744
Alejandro Ortiz <alejandro_ortiz426@hotmail.com>
2024-08-13 08:55:37 -0600

    test

--- resources/views/livewire/entrada/catalogo-entradas/view.blade.php
+++ resources/views/livewire/entrada/catalogo-entradas/view.blade.php
@@ -94,7 +94,7 @@
                     <tr>
                         <td>
                             @if($row->has_parts)
-                                <a href="/servicios/{{$row->id}}?activeTab=5" data-toggle="tooltip" data-placement="top" title="Tiene Refacciones" class="btn btn-xs btn-secondary"><i class="fa fa-wrench"></i></a>
+                                <a href="/servicios/{{$row->id}}?activeTab=5" data-toggle="tooltip" data-placement="top" title="Tiene Refacciones" class="btn btn-xs btn-default"><i class="fa fa-wrench"></i></a>
                             @elseif ($row->check_parts)
                                 <a href="/servicios/{{$row->id}}?activeTab=5" data-toggle="tooltip" data-placement="top" title="Revisar Refacciones" class="btn btn-xs btn-danger"><i class="fa fa-wrench"></i></a>
                             @endif
