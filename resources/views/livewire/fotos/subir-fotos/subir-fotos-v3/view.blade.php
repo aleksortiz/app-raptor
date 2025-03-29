7d6a785b3f529b5ddda61079f81fe36fcf1e5e7a
Alejandro Ortiz <alejandro_ortiz426@hotmail.com>
2025-03-28 18:17:38 -0600

    test

--- resources/views/livewire/fotos/subir-fotos/subir-fotos-v3/view.blade.php
+++ resources/views/livewire/fotos/subir-fotos/subir-fotos-v3/view.blade.php
@@ -72,6 +72,8 @@
         <span class="error text-danger">{{ $message }}</span>
     @enderror
 
+    {{$this->storage_path}}
+
     @if ($images)
         <div class="row justify-content-center">
             @foreach ($images as $image)
@@ -81,6 +83,7 @@
                     </center>
                 </div>
             @endforeach
+            
         </div>
     @endif
 
@@ -89,7 +92,7 @@
             <div class="col-md-2 col-sm-6 m-2">
                 <div class="image-container">
                     <a href="{{$item->url}}" data-toggle="lightbox" data-title="{{$item->fecha_creacion}}" data-gallery="gallery">
-                        <img src="{{$item->location_thumb}}" class="img-fluid mb-2" alt="image"  />
+                        <img src="{{$item->url_thumb}}" class="img-fluid mb-2" alt="image"  />
                     </a>
                     <div class="overlay-buttons">
                         @php
