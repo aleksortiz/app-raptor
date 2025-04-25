25262c48af16a7ee86b3fefbdcf0842f42f84aae
Alejandro Ortiz <alejandro_ortiz426@hotmail.com>
2025-04-25 04:56:02 -0600

    test

--- resources/views/livewire/fotos/subir-fotos/subir-fotos-v3/view.blade.php
+++ resources/views/livewire/fotos/subir-fotos/subir-fotos-v3/view.blade.php
@@ -90,7 +90,7 @@
         @foreach ($this->model?->fotos ?? [] as $item)
             <div class="col-md-2 col-sm-6 m-2">
                 <div class="image-container">
-                    <a href="{{$item->complete_thumb_url}}" data-toggle="lightbox" data-title="{{$item->fecha_creacion}}" data-gallery="gallery">
+                    <a href="{{$item->complete_url}}" data-toggle="lightbox" data-title="{{$item->fecha_creacion}}" data-gallery="gallery">
                         <img src="{{$item->complete_thumb_url}}" class="img-fluid mb-2" alt="image"  />
                     </a>
                     <div class="overlay-buttons">
