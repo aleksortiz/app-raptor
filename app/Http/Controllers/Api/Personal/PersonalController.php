b0094b5c6fbbc2453cfa741317706bca0e95b69e
Alejandro Ortiz <alejandro_ortiz426@hotmail.com>
2025-05-22 12:27:12 -0600

    test

--- app/Http/Controllers/Api/Personal/PersonalController.php
+++ app/Http/Controllers/Api/Personal/PersonalController.php
@@ -129,6 +129,7 @@ class PersonalController extends Controller
 
             return response()->json([
                 'token' => $token,
+                'url' => url('/destajos') . '?token=' . $token,
                 'expires_at' => Carbon::createFromTimestamp($payload['exp'])->toDateTimeString(),
                 'payload' => $payload
             ]);
