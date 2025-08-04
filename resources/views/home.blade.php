14ecf89f62cce32643fa7fac7ab9737be4edf3c1
Alejandro Ortiz <alejandro_ortiz426@hotmail.com>
2025-08-03 23:13:26 -0600

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,48 @@
 @extends('adminlte::page')
 
 @section('content')
+<div class="bg-container">
 @include('shared.system.loader')
 <br><br><br>
 <center>
     <img width="60%" src="{{ asset('images/logo.png') }}">
     {{-- <h1>{{env('APP_FULL_NAME')}}</h1> --}}
-    <h1>Autoservicio Raptor</h1>
-    <h2>{{env('BUSSINESS_DESCRIPTION')}}</h2>
+    <h1 style="color: white;">Autoservicio Raptor</h1>
+    <h2 style="color: white;">{{env('BUSSINESS_DESCRIPTION')}}</h2>
     {{-- <br>
     <br>
     <h3><a href="/aos/tickets-soporte">Solicitud de soporte para errores y/o modificaciones</a></h3> --}}
 </center>
+</div>
+@endsection
+
+@section('css')
+<style>
+    .content-wrapper {
+        background-image: url('{{ asset('images/bg.jpeg') }}');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        position: relative;
+    }
+    
+    .content-wrapper::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: white;
+        opacity: 0.2;
+        z-index: 0;
+    }
+    
+    .bg-container {
+        position: relative;
+        z-index: 1;
+    }
+</style>
 @endsection
 
 @section('js')
